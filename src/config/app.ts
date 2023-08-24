54d3cdf1f593265687014654b0bda4bb5e839358
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-08-24 08:28:47 +0200

    fix: tweak of metadata flow

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,5 +1,14 @@
 export const appConfig = {
-  branding: {
-    name: "Modernized RCC",
+  meta: {
+    title: {
+      branding: "Modernized RCC",
+      separator: " | ",
+      specific: {},
+    },
+    desc: {
+      default:
+        "Modernized RCC - A modernized React.js Class Components based website using Astro.js to attach all components together.",
+      specific: {},
+    },
   },
 };
